014bdbbc5b57981681d6f536b408f34d82b5711f
Bakhadir Rakhmatov <bakhadir.rakhmatov@syf.com>
2022-01-23 15:33:46 -0600

    Voter Info Fragment
    Create BindingAdapter for external urls

--- starter/app/src/main/java/com/example/android/politicalpreparedness/BindingAdapters.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/BindingAdapters.kt
@@ -1,5 +1,22 @@
 package com.example.android.politicalpreparedness
 
-class BindingAdapters {
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.core.content.ContextCompat.startActivity
+import android.net.Uri
+import android.content.Intent
+import android.text.TextUtils
+import android.view.View
 
+@BindingAdapter("externalUrl")
+fun bindUrlToTextView(textView: TextView, url: String?) {
+    if (TextUtils.isEmpty(url)) {
+        textView.visibility = View.GONE
+    } else {
+        textView.visibility = View.VISIBLE
+        textView.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(textView.context, browserIntent, null)
+        }
+    }
 }
