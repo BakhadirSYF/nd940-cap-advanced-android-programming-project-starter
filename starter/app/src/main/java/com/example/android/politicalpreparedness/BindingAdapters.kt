e1176a82892cbcf736b55aa4b3945d4984f7c8e3
Bakhadir Rakhmatov <bakhadir.rakhmatov@syf.com>
2022-01-24 07:40:21 -0600

    Load representative image from url

--- starter/app/src/main/java/com/example/android/politicalpreparedness/BindingAdapters.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/BindingAdapters.kt
@@ -7,6 +7,10 @@ import android.net.Uri
 import android.content.Intent
 import android.text.TextUtils
 import android.view.View
+import android.widget.ImageView
+import androidx.core.net.toUri
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 @BindingAdapter("externalUrl")
 fun bindUrlToTextView(textView: TextView, url: String?) {
@@ -26,5 +30,24 @@ fun bindSavedStateToTextView(textView: TextView, isSaved: Boolean) {
     when (isSaved) {
         true -> textView.setText(R.string.button_label_unfollow_election)
         false -> textView.setText(R.string.button_label_follow_election)
+    }
+}
+
+/**
+ * Use Glide library to load an image from url into an [ImageView]
+ */
+@BindingAdapter("imageUrl")
+fun bindUrlToImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
+                    .circleCrop()
+            )
+            .into(imgView)
     }
 }
